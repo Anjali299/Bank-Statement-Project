a3b611b3d62cd21355e91f24e7825f75a8b792ec
unknown <anjaliroy29999@gmail.com>
2024-03-08 21:00:52 +0530

    first commit

---
+++ src/java/BankAccount/Account.java
@@ -0,0 +1,56 @@
+package BankAccount;
+
+import javax.swing.JOptionPane;
+
+
+public class Account 
+{
+    private double balance;
+    private long accNum;
+    private String accHolder;
+     
+     public Account(String name, long num)
+     {
+    	 accHolder = name;
+         accNum = num;
+         balance = 0.00;
+    	 System.out.println("Account Created");
+     }
+     
+     public void deposit(double amt)
+     {
+    	 balance = balance + amt;
+    	 System.out.println("Amount Deposited.");
+     }
+     
+  public void withdraw(double amt) throws BankAccountException
+     {
+    	 try
+    	 { 
+    	    if(balance >= amt)
+    	    {
+    		    balance = balance - amt;
+        	    System.out.println("Amount Withdrawn");
+    	    }
+    	    else
+    	    {
+                JOptionPane.showMessageDialog(null, "Insuffient Balance", "", 1);
+                System.out.println("Insufficient Balance");
+    		    throw new BankAccountException("Could not Withdraw: Insufficent Amount.");
+    	    }
+    	 }
+    	 catch(BankAccountException bae)
+    	 {
+              bae.printStackTrace();
+         }
+     }
+     
+     public void printBalance()
+     {
+    	 System.out.println("Current balance: " + balance);    	 
+     }
+     public double getBalance()
+     {
+         return balance;
+     }
+}
